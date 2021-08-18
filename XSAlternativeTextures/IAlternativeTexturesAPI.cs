abdb9d1a2600bdbf2b97ec49ee564302df512875
Matthew <justmatt@outlook.com>
2021-08-18 14:47:44 -0500

    Initial version

---
+++ XSAlternativeTextures/IAlternativeTexturesAPI.cs
@@ -0,0 +1,9 @@
+﻿using AlternativeTextures.Framework.Models;
+
+namespace XSAlternativeTextures
+{
+    public interface IAlternativeTexturesAPI
+    {
+        public void AddAlternativeTexture(AlternativeTextureModel model, string owner, string tileSheetPath);
+    }
+}
---
+++ XSAlternativeTextures/IExpandedStorageAPI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace XSAlternativeTextures
+{
+    public interface IExpandedStorageAPI
+    {
+        /// <summary>Returns all Expanded Storage by name.</summary>
+        /// <returns>List of storages</returns>
+        IList<string> GetAllStorages();
+    }
+}
